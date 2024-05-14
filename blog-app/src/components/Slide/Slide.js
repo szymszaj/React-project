a34aaefbda31e0b636f270d16d0d5d33d3906e3d
Szymon Zych <szymonzych868@gmail.com>
2024-05-14 23:24:53 +0200

    ading slide

--- blog-app/src/components/Slide/Slide.js
+++ blog-app/src/components/Slide/Slide.js
@@ -1,9 +1,36 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Slide = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <>
-      <h1>Slide</h1>
+      <div className="slide">
+        <Slider {...settings}>
+          {Sdata.map((value) => {
+            return (
+              <div className="box">
+                <div className="img">
+                  <img src={value.cover} alt="" />
+                </div>
+                <div className="text">
+                  <span>{value.category}</span>
+                  <h2>{value.title}</h2>
+                  <p>{value.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
     </>
   );
 };

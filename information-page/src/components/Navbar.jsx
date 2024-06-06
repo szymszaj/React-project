42fbeb76f12a23f035909a352fff5c44ea481cf7
Szymon Zych <szymonzych868@gmail.com>
2024-06-06 14:37:29 +0200

    navigation creation

---
+++ information-page/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">SH3.</h1>
+      <ul className="hidden md:flex cursor-pointer">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={22} /> : <AiOutlineMenu size={22} />}
+      </div>
+      <ul
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 " 
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">SH3.</h1>
+        <li className="p-4 border-b border-[#121820] cursor-pointer">Home</li>
+        <li className="p-4 border-b border-[#121820] cursor-pointer">Company</li>
+        <li className="p-4 border-b border-[#121820] cursor-pointer">Resources</li>
+        <li className="p-4 border-b border-[#121820] cursor-pointer">About</li>
+        <li className="p-4 cursor-pointer">Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;

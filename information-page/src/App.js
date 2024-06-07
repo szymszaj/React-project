557d9e04369f93ad3f34d46356813e6053f2571a
Szymon Zych <szymonzych868@gmail.com>
2024-06-07 11:12:51 +0200

    adding imports

--- information-page/src/App.js
+++ information-page/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
+      <Cards />
+      <Footer />
     </div>
   );
 }

40072d48260c9879f34ffffc05a530609191f129
Szymon Zych <szymonzych868@gmail.com>
2024-06-08 22:24:23 +0200

    update the files, declarate

--- information-page/src/components/Analytics.jsx
+++ information-page/src/components/Analytics.jsx
@@ -5,7 +5,7 @@ const Analytics = () => {
   return (
     <div className="w-full bg-white py-16  px-4">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <img className='w-[500px] mx-auto my-4' src={Laptop} alt="/" />
+        <img className="w-[500px] mx-auto my-4" src={Laptop} alt="/" />
         <div className="flex flex-col justify-center">
           <p className="text-[#00df9a] font-bold ">ANALYSIS PANEL</p>
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">

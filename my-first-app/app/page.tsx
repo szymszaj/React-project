2306de20ce031627377f31bea2dc49150c63bd1c
Szymon Zych <szymonzych868@gmail.com>
2024-10-03 22:21:52 +0200

    Refactor page component and add Button component

---
+++ my-first-app/app/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { Button } from "../components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
+      <h1 className="text-5xl font-bold">Invoicipedia</h1>
+      <p>
+        <Button asChild>
+          <Link href="/dashboard">Sing in</Link>
+        </Button>
+      </p>
+    </main>
+  );
+}

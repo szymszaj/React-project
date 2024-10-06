b7224558c5f5676a4eab6e2871e77b7f79ee8a54
Szymon Zych <szymonzych868@gmail.com>
2024-10-06 22:00:31 +0200

    add new import

--- my-first-app/app/dashboard/page.tsx
+++ my-first-app/app/dashboard/page.tsx
@@ -8,12 +8,20 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold">Invoices</h1>
+        <p>
+          <Button className="inline-flex gap-3" variant="ghost">
+            <CirclePlus className="h-4 w-4 " />
+            Create Invoice
+          </Button>
+        </p>
       </div>
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>

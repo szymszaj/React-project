f0c591da5af473942481ccca3af533dbe269f5f4
Szymon Zych <szymonzych868@gmail.com>
2024-10-09 21:03:23 +0200

    Add new page component for creating invoices

---
+++ my-first-app/app/invoices/new/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Create a New Invoices</h1>
+      </div>
+    </main>
+  );
+}

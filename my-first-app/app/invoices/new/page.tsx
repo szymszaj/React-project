d6674f9c23caa6b3361223edde12ef422fdfdb93
Szymon Zych <szymonzych868@gmail.com>
2024-10-12 23:31:38 +0200

    Refactor page component and fix formatting in invoice creation page

--- my-first-app/app/invoices/new/page.tsx
+++ my-first-app/app/invoices/new/page.tsx
@@ -4,26 +4,28 @@ import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto my-12">
+    <main className="flex flex-col justify-center h-full  gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold">Create a New Invoices</h1>
       </div>
 
-      <form>
+      <form className="grid gap-4 max-w-xs">
         <div>
-          <Label>Name</Label>
+          <Label className="block font-semibold mb-2 text-sm">Name</Label>
           <Input type="text" />
         </div>
         <div>
-          <Label>Email</Label>
+          <Label className="block mb-2 font-semibold text-sm">Email</Label>
           <Input type="Email" />
         </div>
         <div>
-          <Label>Value</Label>
+          <Label className="block mb-2 font-semibold text-sm">Value</Label>
           <Input type="text" />
         </div>
         <div>
-          <Label>Description</Label>
+          <Label className="block mb-2 font-semibold text-sm">
+            Description
+          </Label>
           <Textarea></Textarea>
         </div>
       </form>

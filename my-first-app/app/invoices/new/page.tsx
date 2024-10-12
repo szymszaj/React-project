adfc2cd8545aeafe22dce9125ea12ad561d560ce
Szymon Zych <szymonzych868@gmail.com>
2024-10-12 23:36:42 +0200

    adding a data panel

--- my-first-app/app/invoices/new/page.tsx
+++ my-first-app/app/invoices/new/page.tsx
@@ -6,27 +6,36 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center h-full  gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
-        <h1 className="text-2xl font-bold">Create a New Invoices</h1>
+        <h1 className="text-2xl font-bold">Create Invoices</h1>
       </div>
 
       <form className="grid gap-4 max-w-xs">
         <div>
-          <Label className="block font-semibold mb-2 text-sm">Name</Label>
-          <Input type="text" />
+          <Label htmlFor="name" className="block font-semibold mb-2 text-sm">
+            Name
+          </Label>
+          <Input id="name" name="name" type="text" />
         </div>
         <div>
-          <Label className="block mb-2 font-semibold text-sm">Email</Label>
-          <Input type="Email" />
+          <Label htmlFor="email" className="block mb-2 font-semibold text-sm">
+            Email
+          </Label>
+          <Input id="email" name="email" type="Email" />
         </div>
         <div>
-          <Label className="block mb-2 font-semibold text-sm">Value</Label>
-          <Input type="text" />
+          <Label htmlFor="value" className="block mb-2 font-semibold text-sm">
+            Value
+          </Label>
+          <Input id="value" name="value" type="text" />
         </div>
         <div>
-          <Label className="block mb-2 font-semibold text-sm">
+          <Label
+            htmlFor="description"
+            className="block mb-2 font-semibold text-sm"
+          >
             Description
           </Label>
-          <Textarea></Textarea>
+          <Textarea id="description" name="description"></Textarea>
         </div>
       </form>
     </main>

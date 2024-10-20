a29630d335c804f35cc12e346aaa0b87ebfcb000
Szymon Zych <szymonzych868@gmail.com>
2024-10-20 22:32:57 +0200

    Refactor background image and address formatting in Home page

--- app-shop/src/app/page.tsx
+++ app-shop/src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-black bg-bg-home bg-cover bg-center">
+    <div className="bg-background bg-bg-home bg-cover bg-center">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
         <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
           <h1 className="text-4xl font-bold">
@@ -10,8 +10,7 @@ export default function Home() {
             <br /> Repair Shop
           </h1>
           <address>
-            555 Main St.
-            <br />
+            555 Main St. <br />
             Anytown, NY 12345
           </address>
           <p>Open Monday-Friday</p>

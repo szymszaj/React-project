86262a75cd5bc73f7fb4ce8406c923528dd90a5b
Szymon Zych <szymonzych868@gmail.com>
2025-01-03 18:29:08 +0100

    Add ImageComponent for responsive image handling

---
+++ data-retrieval/src/app/components/ui/image/image.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+interface ImageProps {
+  readonly imageDesktop: string;
+  readonly imageMobile?: string;
+  readonly altText?: string;
+  readonly width?: number;
+  readonly height?: number;
+  readonly className?: string;
+  readonly quality?: number;
+  readonly priority?: boolean;
+  readonly layout?: "fill" | "fixed" | "intrinsic" | "responsive";
+  readonly sizes?: string;
+  readonly fill?: boolean;
+  readonly objectFit?: "contain" | "cover";
+}
+
+function ImageComponent({
+  imageDesktop,
+  imageMobile,
+  altText,
+  width,
+  height,
+  className,
+}: ImageProps) {
+  return (
+    <div className={`relative w-full ${className}`}>
+      <div className="relative hidden w-full lg:block">
+        <Image
+          src={imageDesktop}
+          alt={altText || ""}
+          width={width}
+          height={height}
+          className="object-cover"
+        />
+      </div>
+
+      {imageMobile && (
+        <div className="relative block w-full lg:hidden">
+          <Image
+            src={imageMobile}
+            alt={altText || ""}
+            width={width}
+            height={height}
+            className="object-cover"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export { ImageComponent };

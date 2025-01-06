abffe602a0a853b57fdd57f632df2dd0607f2b9a
Szymon Zych <szymonzych868@gmail.com>
2025-01-06 22:30:41 +0100

    Integrate Accordion component to enhance section display and user interaction

--- data-retrieval/src/app/page.tsx
+++ data-retrieval/src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import { sections } from "@/components/data/sections";
 import { TitlesNav } from "@/components/ui/titleNav";
 import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/ui/accordion";
 
 export default function HomePage() {
   return (
@@ -8,15 +16,21 @@ export default function HomePage() {
       <TitlesNav sections={sections} />
 
       <div className="p-5">
-        {sections.map((section) => (
-          <div key={section.id} id={section.id} className="my-10">
-            <h2 className="text-2xl font-bold">{section.title}</h2>
-            <p className="mt-2 text-gray-900">{section.content}</p>
-            <Button variant="default" size="default" className="mt-4">
-              ok!
-            </Button>
-          </div>
-        ))}
+        <Accordion type="single" collapsible>
+          {sections.map((section) => (
+            <AccordionItem key={section.id} value={section.id}>
+              <AccordionTrigger>
+                <h2 className="text-2xl font-bold">{section.title}</h2>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p className="mt-2 text-gray-900">{section.content}</p>
+                <Button variant="default" size="default" className="mt-4">
+                  ok!
+                </Button>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
     </div>
   );

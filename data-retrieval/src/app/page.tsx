8c779d255bd0d62c6e739e5a23acca97c4d9efcc
Szymon Zych <szymonzych868@gmail.com>
2025-01-06 22:24:33 +0100

    Add Button component to HomePage for improved user interaction

--- data-retrieval/src/app/page.tsx
+++ data-retrieval/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { sections } from "@/components/data/sections";
 import { TitlesNav } from "@/components/ui/titleNav";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
@@ -11,6 +12,9 @@ export default function HomePage() {
           <div key={section.id} id={section.id} className="my-10">
             <h2 className="text-2xl font-bold">{section.title}</h2>
             <p className="mt-2 text-gray-900">{section.content}</p>
+            <Button variant="default" size="default" className="mt-4">
+              ok!
+            </Button>
           </div>
         ))}
       </div>

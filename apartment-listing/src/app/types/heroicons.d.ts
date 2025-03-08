4d34813528c9ae0007fbddd0bbe809b8312ccc9c
Szymon Zych <szymonzych868@gmail.com>
2025-03-08 22:45:37 +0100

    Remove unused Heroicons type definitions to clean up codebase

--- apartment-listing/src/app/types/heroicons.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@heroicons/react/outline" {
-  import { FunctionComponent, SVGProps } from "react";
-
-  export const FaInstagram: FunctionComponent<SVGProps<SVGSVGElement>>;
-  export const FaFacebook: FunctionComponent<SVGProps<SVGSVGElement>>;
-  export const FaAirbnb: FunctionComponent<SVGProps<SVGSVGElement>>;
-}
